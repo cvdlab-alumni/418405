48a41c34b2282b6333f77516896a4f351d9dbb74
antonellogatto <antonello.gatto@gmail.com>
2013-04-06 18:51:29 +0200

    terminato l'hw1

--- 2013-04-05/python/exercise5.py
+++ 2013-04-05/python/exercise5.py
@@ -1,7 +1,21 @@
 from pyplasm import *
 
-step2D = MKPOL([[[0,0],[0,1.4+raiser],[depth,raiser],[depth,1.4+raiser]],
-    [[1,2,3,4]],None])
-step3D = MAP([S1,S3,S2])(PROD([step2D,Q(9)]))
+''' step '''
 
-ramp = STRUCT(NN(9)([step3D,T([1,3])([depth,raiser])]))
+step2D = MKPOL([[[0,0],[0.50,0.19],[0.5,0.33],[0,0.33]],[[1,2,3,4]],None])
+
+step3D = MAP([S1,S3,S2])(PROD([step2D,Q(1.8)]))
+
+''' ramp '''
+
+ramp_origin = STRUCT(NN(13)([step3D, T([1,3])([0.50,0.19])]))
+
+stair2 = T([1,2,3])([4.8,9.7+0.4,2.64])(ramp_origin)
+
+stair3 = T([1,2,3])([8.8,9.7+0.4,2.64+2.5])(ramp_origin)
+
+stair1 = T([1,2,3])([5.5,9.7+0.4,0.14])(R([1,2])(PI*2)(ramp_origin))
+
+stairs = STRUCT([stair1,stair2,stair3])
+
+building = STRUCT([pillars,floors,vert_partitions,windows,stairs])
